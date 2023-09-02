77de20c49a1e15363d2e95cdc00da2cfee06780a
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2023-09-02 14:36:06 -0500

    mount: use time-based "generations" for secrets dir

--- src/system/mod.rs
+++ src/system/mod.rs
@@ -2,6 +2,8 @@ use std::collections::HashMap;
 use std::path::Path;
 
 use age::Identity;
+use nix::sys::time::TimeValLike;
+use nix::time::{clock_gettime, ClockId};
 use tokio::fs;
 use tokio::process::Command;
 
@@ -23,7 +25,7 @@ mod linux;
 pub use linux::*;
 
 pub async fn mount<S: SecretStorage>(
-    mount_point: &Path,
+    base_mount_point: &Path,
     secret_dir: &Path,
     secrets: &HashMap<String, Secret>,
     exposures: &HashMap<String, Vec<FileExposeArgs>>,
@@ -33,24 +35,37 @@ pub async fn mount<S: SecretStorage>(
 where
     <S as SecretStorage>::Error: 'static,
 {
-    // TODO: Need to bring in directory work from other branch
-    if device_mounted(secret_dir)? {
+    // Get time since boot in ms
+    let time_ms = clock_gettime(ClockId::CLOCK_MONOTONIC)
+        .expect("failed to get time of day")
+        .num_milliseconds()
+        .to_string();
+    let mount_point = base_mount_point.join(time_ms);
+
+    // NOTE: Because we mount a tmpfs, and use the ms since boot in our
+    // generation directory, it is highly unlikely that we will run into a
+    // collision here. If we do, there's likely some kind of crafted
+    // timing attack going on, and we shouldn't write any secrets here.
+    // If the directory exists, but isn't mounted, then we'll write to our
+    // tmpfs without writing to whatever is currently backing this
+    // directory anyway.
+    if device_mounted(&mount_point)? {
         return Err(MountSecretsError::AlreadyMounted);
     }
 
-    if !secret_dir.exists() {
-        let _ = fs::create_dir(secret_dir)
+    if !mount_point.exists() {
+        let _ = fs::create_dir(&mount_point)
             .await
             .map_err(MountSecretsError::CreatingFilesFailure);
     }
 
-    mount_persistent_ramfs(secret_dir).map_err(MountSecretsError::RamfsCreationFailure)?;
+    mount_persistent_ramfs(&mount_point).map_err(MountSecretsError::RamfsCreationFailure)?;
     let file_pairs =
         map_secrets(secrets, exposures.iter()).map_err(MountSecretsError::NoSuchSecret)?;
 
-    expose_files(secret_dir, storage, &file_pairs, identities).await?;
+    expose_files(&mount_point, storage, &file_pairs, identities).await?;
 
-    tokio::fs::symlink(mount_point, secret_dir)
+    tokio::fs::symlink(&mount_point, secret_dir)
         .await
         .map_err(MountSecretsError::SymlinkCreationFailure)?;
 
