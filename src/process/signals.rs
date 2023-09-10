c13222ed768f81ca7e51a22c34d09831a7bc30b6
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2023-09-09 17:24:05 -0700

    process signals: handle fewer signals

--- src/process/signals.rs
+++ src/process/signals.rs
@@ -4,42 +4,8 @@ use nix::libc::SIGEMT;
 use signal_hook::consts::*;
 use tokio::process::Command;
 
-#[cfg(not(target_os = "macos"))]
-const NUM_SIGNALS: usize = 24;
-
-#[cfg(target_os = "macos")]
-const NUM_SIGNALS: usize = 26;
-
-// All signals we can legally handle
-pub const SIGNALS: [i32; NUM_SIGNALS] = [
-    SIGHUP,
-    SIGINT,
-    SIGQUIT,
-    SIGTRAP,
-    SIGABRT,
-    #[cfg(target_os = "macos")]
-    SIGEMT,
-    SIGBUS,
-    SIGSYS,
-    SIGPIPE,
-    SIGALRM,
-    SIGTERM,
-    SIGURG,
-    SIGTSTP,
-    SIGCONT,
-    SIGCHLD,
-    SIGTTIN,
-    SIGTTOU,
-    SIGIO,
-    SIGXCPU,
-    SIGXFSZ,
-    SIGVTALRM,
-    SIGPROF,
-    SIGWINCH,
-    #[cfg(target_os = "macos")]
-    SIGINFO,
-    SIGUSR1,
-    SIGUSR2,
+pub const SIGNALS: [i32; 9] = [
+    SIGHUP, SIGINT, SIGQUIT, SIGABRT, SIGTERM, SIGTSTP, SIGCONT, SIGUSR1, SIGUSR2,
 ];
 
 pub async fn kill(pid: u32, signal: i32) -> Result<(), std::io::Error> {
